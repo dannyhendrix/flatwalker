e3d44bf2fe951260ca4f454459f343e7e9c4476d
Danny Hendrix <dwm.hendrix@gmail.com>
2025-05-09 23:59:51 -0700

    player animation

---
+++ game/src/animation.ts
@@ -0,0 +1,38 @@
+
+export class Animation{
+    frameHeight:number = 39
+    frameWidth:number = 22
+    frameTime:number = 6
+    lastFrameTime:number = 0
+    spriteSheet: HTMLImageElement
+    currentAnimation: number[][] = []
+    currentFrame: number = 0
+    constructor(){
+        this.spriteSheet = new Image()
+        this.spriteSheet.src = "player.png"
+    }
+    draw(ctx:CanvasRenderingContext2D,x:number,y:number,animation:number[][]){
+        if(animation != this.currentAnimation){
+            console.log("animation diff")
+            this.lastFrameTime = 0
+            this.currentFrame = 0
+            this.currentAnimation = animation
+        }else{
+            this.lastFrameTime += 1
+        }
+        if(this.lastFrameTime >= this.frameTime){
+            this.currentFrame = (this.currentFrame + 1) % animation.length
+            this.lastFrameTime = 0
+        }
+        var indexX = animation[this.currentFrame][0]
+        var indexY = animation[this.currentFrame][1]
+        ctx.drawImage(
+            this.spriteSheet,
+            indexX * this.frameWidth, indexY * this.frameHeight, // Source X, Y (sprite row)
+            this.frameWidth, this.frameHeight,   // Source width, height
+            x, y,                      // Destination X, Y
+            this.frameWidth, this.frameHeight    // Destination width, height
+        );
+    }
+    // 286/13 x 156/4
+}
--- game/src/player.ts
+++ game/src/player.ts
@@ -1,31 +1,46 @@
 import * as input from "./input.js"
 import * as game from "./game.js"
+import * as animation from "./animation.js"
 
-export enum PlayerState {
-    Idle, WalkUp, WalkDown, WalkLeft, WalkRight
+export enum PlayerDirection {
+    Up, Down, Left, Right
 }
+
+const ANIMATION_IDLE_UP = [[0,0],[1,0],[2,0]]
+const ANIMATION_IDLE_DOWN = [[0,2],[1,2],[2,2]]
+const ANIMATION_IDLE_LEFT = [[0,1],[1,1],[2,1]]
+const ANIMATION_IDLE_RIGHT = [[0,3],[1,3],[2,3]]
+const ANIMATION_WALK_UP = [[3,0],[4,0],[5,0]]
+const ANIMATION_WALK_DOWN = [[3,2],[4,2],[5,2]]
+const ANIMATION_WALK_LEFT = [[3,1],[4,1],[5,1]]
+const ANIMATION_WALK_RIGHT = [[3,3],[4,3],[5,3]]
+const ANIMATION_DIE = [[12,0],[12,1],[12,2]]
 
 export class Player implements game.GameUpdatable {
     x: number;
     y: number;
     speed: number = 2
-    runspeed: number = 6
-    state: PlayerState = PlayerState.Idle
+    runspeed: number = 5
+    state_direction: PlayerDirection = PlayerDirection.Down
+    state_isMoving = false
     state_isRunning = false
+    animation: animation.Animation
 
     constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
+        this.animation = new animation.Animation()
     }
 
     update(mygame:game.Game, deltaTime:number) {
         this.setPlayerState(mygame.input)
-        this.handlePlayerState(this.state)
+        this.handlePlayerState()
     }
 
     draw(ctx: CanvasRenderingContext2D) {
         ctx.fillStyle = "blue";
         ctx.fillRect(this.x, this.y, 20, 20);
+        this.animation.draw(ctx,this.x,this.y, this.getAnimationFromState())
     }
     
     setPlayerState(controleInput:input.Input) {
@@ -34,37 +49,62 @@ export class Player implements game.GameUpdatable {
         var indexLeft = controleInput.keys.indexOf(input.GameControls.MoveLeft)
         var indexRight = controleInput.keys.indexOf(input.GameControls.MoveRight)
         var highest = Math.max(indexUp,indexDown,indexLeft,indexRight)
-        this.state = this.getPlayerStateFromControl(controleInput.keys[highest])
+        if(highest > -1)
+            this.state_direction = this.getPlayerDirectionFromControl(controleInput.keys[highest])
         this.state_isRunning = controleInput.isKeyPressed(input.GameControls.Run)
+        this.state_isMoving = highest > -1
     }
 
-    getPlayerStateFromControl(control:input.GameControls){
+    getPlayerDirectionFromControl(control:input.GameControls){
         switch(control){
-            case input.GameControls.MoveDown: return PlayerState.WalkDown
-            case input.GameControls.MoveUp: return PlayerState.WalkUp
-            case input.GameControls.MoveLeft: return PlayerState.WalkLeft
-            case input.GameControls.MoveRight: return PlayerState.WalkRight
+            case input.GameControls.MoveDown: return PlayerDirection.Down
+            case input.GameControls.MoveUp: return PlayerDirection.Up
+            case input.GameControls.MoveLeft: return PlayerDirection.Left
+            case input.GameControls.MoveRight: return PlayerDirection.Right
         }
-        return PlayerState.Idle
+        return PlayerDirection.Down
     }
 
-    handlePlayerState(state:PlayerState){
+    handlePlayerState(){
+        if(!this.state_isMoving) 
+            return
         var walkspeed = this.speed
         if(this.state_isRunning) 
             walkspeed = this.runspeed
-        switch(state){
-            case PlayerState.WalkDown:
+        switch(this.state_direction){
+            case PlayerDirection.Down:
                 this.y += walkspeed
                 break
-            case PlayerState.WalkUp:
+            case PlayerDirection.Up:
                 this.y -= walkspeed
                 break
-            case PlayerState.WalkLeft:
+            case PlayerDirection.Left:
                 this.x -= walkspeed
                 break
-            case PlayerState.WalkRight:
+            case PlayerDirection.Right:
                 this.x += walkspeed
                 break
+        }
+    }
+
+    getAnimationFromState(){
+        switch(this.state_direction){
+            case PlayerDirection.Down:
+                if(this.state_isMoving) 
+                    return ANIMATION_WALK_DOWN
+                return ANIMATION_IDLE_DOWN
+            case PlayerDirection.Up:
+                if(this.state_isMoving) 
+                    return ANIMATION_WALK_UP
+                return ANIMATION_IDLE_UP
+            case PlayerDirection.Left:
+                if(this.state_isMoving) 
+                    return ANIMATION_WALK_LEFT
+                return ANIMATION_IDLE_LEFT
+            case PlayerDirection.Right:
+                if(this.state_isMoving) 
+                    return ANIMATION_WALK_RIGHT
+                return ANIMATION_IDLE_RIGHT
         }
     }
 }
